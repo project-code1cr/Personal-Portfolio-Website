1306b530f979d83a99ee3f0e8e9bfc4d97e788a1
project-code1cr <amangupta125er@gmail.com>
2025-07-01 23:01:42 +0530

    changes

--- src/components/Navbar/navbar.js
+++ src/components/Navbar/navbar.js
@@ -13,7 +13,7 @@ const Navbar = () => {
       <Link activeClass ='active' to ='intro' spy={true} smooth={true} offset={-100} duration={500} className ='desktopMenuListItem'>Home</Link>
       <Link activeClass ='active' to ='skills' spy={true} smooth={true} offset={-50} duration={500} className ='desktopMenuListItem'>About</Link>
       <Link activeClass ='active' to ='works' spy={true} smooth={true} offset={-50} duration={500} className ='desktopMenuListItem'>Portfolio</Link>
-      <Link activeClass ='active' to ='clients' spy={true} smooth={true} offset={-50} duration={500} className ='desktopMenuListItem'>Clients</Link>
+      <Link activeClass ='active' to ='clients' spy={true} smooth={true} offset={-50} duration={500} className ='desktopMenuListItem'>Coding Profiles</Link>
     </div>
     <button className='desktopMenuBtn' onClick={() =>{
       document.getElementById('contact').scrollIntoView({behavior:'smooth'});
